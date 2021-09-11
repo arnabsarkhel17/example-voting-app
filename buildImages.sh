0de442f5cacbcaa6ebdfec47e7aa5f1613469dd4
Ubuntu <azureuser@AmadeusTraining.yapqf2j5rn5utplrx4m5qtwvig.bx.internal.cloudapp.net>
2021-09-11 17:40:42 +0000

    Updated docker-compose buildimages

--- buildImages.sh
+++ buildImages.sh
@@ -8,3 +8,8 @@ docker build -t arnabsarkhel/result-app .
 docker push arnabsarkhel/result-app
 
 cd ..
+cd worker
+docker build -t arnabsarkhel/worker-app .
+docker push arnabsarkhel/worker-app
+
+cd ..
